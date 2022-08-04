cf676f730624b4272baaed4bdf06b39066c23374
AntiStC <94138193+AntiStC@users.noreply.github.com>
2022-08-04 12:58:33 +0300

    Initial commit

---
+++ src/test/java/ru/sspk/ssdmd/AetsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sspk.ssdmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
