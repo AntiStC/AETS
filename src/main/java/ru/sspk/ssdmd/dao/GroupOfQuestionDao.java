1572233c2d813307858cf83164570ae4f9d67377
Lyubarskikh Kirill <k.lubarskih@mail.ru>
2022-08-05 15:07:50 +0300

    Created test dao-s

---
+++ src/main/java/ru/sspk/ssdmd/dao/GroupOfQuestionDao.java
@@ -0,0 +1,6 @@
+package ru.sspk.ssdmd.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupOfQuestionDao extends JpaRepository<GroupQuestion, Long> {
+}
---
+++ src/main/java/ru/sspk/ssdmd/dao/TestDao.java
@@ -0,0 +1,7 @@
+package ru.sspk.ssdmd.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestDao extends JpaRepository<Test, Long> {
+
+}
