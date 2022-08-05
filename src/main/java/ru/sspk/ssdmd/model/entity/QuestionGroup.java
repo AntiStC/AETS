1ff6d8e3961fce170e2b3a7c3f77dad65aa86322
AntiStC <94138193+AntiStC@users.noreply.github.com>
2022-08-05 10:32:21 +0300

    edit QuestionGroup entity

--- src/main/java/ru/sspk/ssdmd/model/entity/QuestionGroup.java
+++ src/main/java/ru/sspk/ssdmd/model/entity/QuestionGroup.java
@@ -3,8 +3,9 @@ package ru.sspk.ssdmd.model.entity;
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.Objects;
 
-@Table(name = "question_group",catalog = "public",schema = "sspk_aets")
+@Table(name = "question_group", catalog = "public", schema = "sspk_aets")
 @Entity
 public class QuestionGroup {
 
@@ -16,5 +17,81 @@ public class QuestionGroup {
     @OneToMany
     private List<Question> questionList;
 
-    //TODO builder, getter, setter, construction, toString
+    public static class Builder {
+
+        private Long id;
+        private String name;
+        private List<Question> questionList;
+
+        public Builder setId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setQuestionList(List<Question> questionList) {
+            this.questionList = questionList;
+            return this;
+        }
+
+        public QuestionGroup build() {
+            return new QuestionGroup(this);
+        }
+    }
+
+    private QuestionGroup(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.questionList = builder.questionList;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Question> getQuestionList() {
+        return questionList;
+    }
+
+    public void setQuestionList(List<Question> questionList) {
+        this.questionList = questionList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        QuestionGroup that = (QuestionGroup) o;
+        return name.equals(that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "QuestionGroup{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", questionList=" + questionList +
+                '}';
+    }
 }
