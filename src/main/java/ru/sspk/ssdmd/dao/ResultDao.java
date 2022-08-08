9e85161099bcec5871573afc7a28427eaa9424ad
Lyubarskikh Kirill <k.lubarskih@mail.ru>
2022-08-08 09:48:12 +0300

    Changed parameters in extend interface JpaRepository

--- src/main/java/ru/sspk/ssdmd/dao/ResultDao.java
+++ src/main/java/ru/sspk/ssdmd/dao/ResultDao.java
@@ -1,6 +1,7 @@
 package ru.sspk.ssdmd.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import ru.sspk.ssdmd.model.entity.Result;
 
 public interface ResultDao extends JpaRepository<Result, Long> {
 }
--- src/main/java/ru/sspk/ssdmd/dao/TestDao.java
+++ src/main/java/ru/sspk/ssdmd/dao/TestDao.java
@@ -1,6 +1,7 @@
 package ru.sspk.ssdmd.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import ru.sspk.ssdmd.model.entity.Test;
 
 public interface TestDao extends JpaRepository<Test, Long> {
 
