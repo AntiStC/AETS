cd97288b03d4b481eeb45cf1c4b01d51ec21e314
Lyubarskikh Kirill <k.lubarskih@mail.ru>
2022-08-08 10:59:32 +0300

    Added to UserDao "findByLogin"

--- src/main/java/ru/sspk/ssdmd/dao/UserDao.java
+++ src/main/java/ru/sspk/ssdmd/dao/UserDao.java
@@ -3,5 +3,8 @@ package ru.sspk.ssdmd.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.sspk.ssdmd.model.entity.User;
 
+import java.util.List;
+
 public interface UserDao extends JpaRepository<User, Long> {
+    List<User> findByLogin(String userName);
 }
