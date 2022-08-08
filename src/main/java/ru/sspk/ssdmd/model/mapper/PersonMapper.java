bae707caa4d94888171cbffe7dd41d35cc086b29
AntiStC <94138193+AntiStC@users.noreply.github.com>
2022-08-08 17:52:09 +0300

    fixit! create toList & return in toEntity & toDto

--- src/main/java/ru/sspk/ssdmd/model/mapper/PersonMapper.java
+++ src/main/java/ru/sspk/ssdmd/model/mapper/PersonMapper.java
@@ -5,13 +5,15 @@ import ru.sspk.ssdmd.model.dto.UserDto;
 import ru.sspk.ssdmd.model.entity.Person;
 import ru.sspk.ssdmd.model.entity.User;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PersonMapper {
-    public static PersonDto toDto(Person person){
+    public static PersonDto toDto(Person person) {
         List<UserDto> userDtoList = person.getUsers().stream()
                 .map(UserMapper::toDto).toList();
-        return new PersonDto.Builder()
+        PersonDto personDto = new PersonDto.Builder()
                 .setId(person.getId())
                 .setFirstName(person.getFirstName())
                 .setLastName(person.getLastName())
@@ -21,12 +23,14 @@ public class PersonMapper {
                 .setChief(person.getChief())
                 .setUserDtos(userDtoList)
                 .build();
+
+        return personDto;
     }
 
-    public static Person toEntity(PersonDto personDto){
+    public static Person toEntity(PersonDto personDto) {
         List<User> userList = personDto.getUserDtos().stream()
                 .map(UserMapper::toEntity).toList();
-        return new Person.Builder()
+        Person person = new Person.Builder()
                 .setId(personDto.getId())
                 .setFirstName(personDto.getFirstName())
                 .setLastName(personDto.getLastName())
@@ -36,5 +40,12 @@ public class PersonMapper {
                 .setChief(personDto.getChief())
                 .setUsers(userList)
                 .build();
+
+        return person;
+    }
+
+    public static List<PersonDto> toListPerson(List<Person> personList) {
+        return new ArrayList<>((personList).stream().map(PersonMapper::toDto)
+                .collect(Collectors.toList()));
     }
 }
