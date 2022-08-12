57bd2bad152773b37685bf52f1999958140adf71
Lyubarskikh Kirill <k.lubarskih@mail.ru>
2022-08-12 10:51:42 +0300

    Added getByPersonListUsersLogin in ResultService

--- src/main/java/ru/sspk/ssdmd/service/ResultService.java
+++ src/main/java/ru/sspk/ssdmd/service/ResultService.java
@@ -4,7 +4,10 @@ import ru.sspk.ssdmd.model.dto.PersonDto;
 import ru.sspk.ssdmd.model.dto.ResultDto;
 import ru.sspk.ssdmd.model.dto.TestDto;
 
+import java.util.List;
+
 public interface ResultService {
     ResultDto generateResult(TestDto testDto, PersonDto personDto);
     ResultDto saveResult(ResultDto resultDto);
+    List<ResultDto> getByPersonListUsersLogin(String login);
 }
